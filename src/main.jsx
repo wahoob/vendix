90f1730639c27b12d43d503d7ea64ee84facf14f
wahoob <majed.eddin.wahoub@gmail.com>
2025-07-03 14:31:53 +0300

    Fix error

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,11 @@ import { Provider } from "react-redux";
 import "./styles/global.css";
 import { store } from "./app/store.js";
 import App from "./app/App.jsx";
-import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
-      <Analytics />
     </Provider>
   </StrictMode>,
 );

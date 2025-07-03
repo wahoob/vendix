779ab9839a760f9992cab2d9000add6d99553015
wahoob <majed.eddin.wahoub@gmail.com>
2025-07-03 14:35:33 +0300

    start counting visitors and page views on vercel for react

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./styles/global.css";
 import { store } from "./app/store.js";
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </StrictMode>,
 );
